3f4817087d6df34ddcbf88fa655915e1aed7ba0d
hiraginoyuki <yuki@shiina.family>
2023-11-02 23:16:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,11 @@
 
 if [ ! -e "${ZIM_HOME}/zimfw.zsh" ]; then
   if command -v curl > /dev/null; then
-    sh -c "$(curl -fsSL get.chezmoi.io/lb)" -- init --apply hiraginoyuki
+    BINDIR="$HOME/.local/bin" sh -c "$(curl -fsS https://get.chezmoi.io/)" -- init --apply hiraginoyuki
   elif command -v wget > /dev/null; then
-    sh -c "$(wget -nv -O- get.chezmoi.io/lb)" -- init --apply hiraginoyuki
+    BINDIR="$HOME/.local/bin" sh -c "$(wget -nv -O- https://get.chezmoi.io/)" -- init --apply hiraginoyuki
   else
     echo 'To run this script, either `curl` or `wget` is required.'
     exit 1
   fi
 fi
-
